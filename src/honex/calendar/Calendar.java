db35025460376bc7e99bb9aa3d19677e162d4aa2
Lucas ko <Lucas ko@DESKTOP-9M1BHF9>
2021-03-06 23:08:43 +0900

    add input first weekday + print each 7 days

--- src/honex/calendar/Calendar.java
+++ src/honex/calendar/Calendar.java
@@ -4,10 +4,45 @@ import java.util.Scanner;
 
 public class Calendar {
 
-	private static final int[] MAX_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31} ;
-	
-	public static int getMaxDaysOfMonth(int mon) { 
-		
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public static int intWeekDay(String weekday) {
+		switch (weekday) {
+		case "SU":
+			return (0);
+		case "MO":
+			return (1);
+		case "TU":
+			return (2);
+		case "WE":
+			return (3);
+		case "TH":
+			return (4);
+		case "FR":
+			return (5);
+		case "SA":
+			return (6);
+		default:
+			return (-1);
+		}
+	}
+
+	private static Boolean isLeapYear(int year) {
+		if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+			return true;
+		} else if (year % 4 == 0 && year % 100 == 0) {
+			return false;
+		} else if (year % 4 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public static int getMaxDaysOfMonth(int year, int mon) {
+
 //		switch(mon) { 
 //		case 1 :
 //			return 31;
@@ -16,20 +51,42 @@ public class Calendar {
 //		default : 
 //			return 30;
 //		}
-		return MAX_DAYS[mon-1];
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[mon - 1];
+		} else {
+			return MAX_DAYS[mon - 1];
+		}
 	}
-	
-	public void printCalendar(int year, int month) { 
-		System.out.printf("<< %4d년%3d월 >> \n",year,month);
+
+	public void printCalendar(int year, int month, int weekdayint) {
+
+		int cnt=0;
+		System.out.println("weekday int=> " + weekdayint);
+		System.out.printf("<< %4d년%3d월 >> \n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
-		;
-		for(int i=0; i<getMaxDaysOfMonth(month); i++) {
-			System.out.printf("%3d",i+1);
-			if((i+1) % 7 == 0) {
-				System.out.println();
-			}
+
+		for (int j = 0; j < weekdayint; j++) {
+			System.out.print("   ");
 		}
+		
+		cnt = weekdayint+1;
+				
+		for (int i = 0; i < getMaxDaysOfMonth(year, month) ; i++) {
+				System.out.printf("%3d", i + 1);
+				if ((cnt) % 7 == 0) {
+					System.out.println();
+					cnt=0;
+				}
+				cnt++;
+		}	
+
+//		for (int i = 0; i < getMaxDaysOfMonth(year, month); i++) {
+//			System.out.printf("%3d", i + 1);
+//			if ((i + 1) % 7 == 0) {
+//				System.out.println();
+//			}
+//		}
 		System.out.println("\n");
 //		System.out.println(" 1	 2	 3	 4	 5	 6	 7");
 //		System.out.println(" 8	 9	10	11	12	13	14");
@@ -37,6 +94,5 @@ public class Calendar {
 //		System.out.println("22	23	24	25	26	27	28");
 //		System.out.println("29	30	31		      ");
 	}
-	
 
 }
--- src/honex/calendar/Prompt.java
+++ src/honex/calendar/Prompt.java
@@ -2,10 +2,18 @@ package honex.calendar;
 
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Lucas ko
+ * # 입력된 월의 1일의 요일을 입력 받는다. 
+ * # 입력받은 요일 부터 1일이 출력되어 말일까지 주별로 7일씩 출력되도록 작성.
+ */
+
 public class Prompt {
 	
 	private final static String PROMPT_YEAR="YEAR> ";
 	private final static String PROMPT_MONTH="MONTH> ";
+	private final static String PROMPT_WEEKDAY="WEEKDAY(SU,MO,TU,WE,TH,FR,SA)> ";
 	
 	public void runPrompt() { 
 		//숫자 입력받아 그달의 최대일수 출력
@@ -15,16 +23,29 @@ public class Prompt {
 		while(true) { 
 			System.out.print(PROMPT_YEAR);
 			int year = scanner.nextInt();
+			if (year == -1) {
+				System.out.println("종료합니다.-EXIT");
+				break;
+			}
 			System.out.print(PROMPT_MONTH);
-			int mon = scanner.nextInt();
-			if (mon==-1) { 
+			int month = scanner.nextInt();
+			if (month<1 || month >12) { 
+				System.out.println("잘못된 월을 입력했습니다");
 				break;
 			} 
-			if (mon>12) { 
+			if (month>12) { 
 				continue;
 			}
+			System.out.print(PROMPT_WEEKDAY);
+			String weekday = scanner.next();
+			int weekdayint = cal.intWeekDay(weekday.toUpperCase());
+			if (weekdayint==-1) {
+				System.out.println("잘못된 요일을 입력했습니다");
+				break;
+			}
+			
 //			System.out.printf("%d 월의 최대 일수는 %d 입니다. \n", mon, cal.getMaxDaysOfMonth(mon));
-			cal.printCalendar(year, mon);
+			cal.printCalendar(year, month, weekdayint);
 		}
 		
 		System.out.println("Bye!!" );	
