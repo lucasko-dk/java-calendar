33188dc1934a27b52932be51fb4d10ce5a118367
Lucas ko <Lucas ko@DESKTOP-9M1BHF9>
2021-03-07 02:00:04 +0900

    input year, month only & input weekday automatically

--- src/honex/calendar/Calendar.java
+++ src/honex/calendar/Calendar.java
@@ -1,11 +1,67 @@
 package honex.calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+//	public static void main(String[] args) {
+//		Calendar calendar = new Calendar();
+//		int result = calendar.getWeekDay(1970, 2,1);
+//	}
+
+	public static int getWeekDay(int year, int month, int day) {
+		int sYear = 1970;
+		int sMonth = 1;
+		int sDay = 0;
+		final int STANDARD_WEEKDAY = 3; // 1970/1/1 = thursday
+
+		// year day
+		if (year - sYear > 0) {
+
+			for (int i = 0; i < year - sYear; i++) {
+
+				if (isLeapYear(year - i - 1)) {
+					sDay = sDay + 366;
+				} else {
+					sDay = sDay + 365;
+				}
+//				System.out.println("year Days=> " + sDay);
+			}
+
+		}
+
+		// month day
+
+		for (int i = 0; i < month-1; i++) {
+			if (isLeapYear(year)) {
+				sDay = sDay + LEAP_MAX_DAYS[i];
+			} else {	
+				sDay = sDay + MAX_DAYS[i];
+			}
+//			System.out.println("Month Days=> " + sDay);			
+		}
+		
+		//day
+		sDay=sDay+day;
+//		System.out.println("sday=> " + sDay);
+		
+		int weekday ; 
+		//week day 
+		if (year==1970 && month==1) { 
+			weekday = STANDARD_WEEKDAY;
+		} else {  
+			weekday = (sDay) % 7;
+			weekday = weekday+(STANDARD_WEEKDAY-1);
+		}	
+		
+//		System.out.println("total day= " + sDay);
+//		System.out.println("week day= " + weekday);
+//		System.out.println("weekday " + weekday);
+		
+		if (weekday==7) { weekday=0; }
+		return weekday;
+	}
 
 	public static int intWeekDay(String weekday) {
 		switch (weekday) {
@@ -60,26 +116,26 @@ public class Calendar {
 
 	public void printCalendar(int year, int month, int weekdayint) {
 
-		int cnt=0;
-		System.out.println("weekday int=> " + weekdayint);
+		int cnt = 0;
+//		System.out.println("weekday int=> " + weekdayint);
 		System.out.printf("<< %4d년%3d월 >> \n", year, month);
-		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println(" MO TU WE TH FR SA SU");
 		System.out.println("---------------------");
 
 		for (int j = 0; j < weekdayint; j++) {
 			System.out.print("   ");
 		}
-		
-		cnt = weekdayint+1;
-				
-		for (int i = 0; i < getMaxDaysOfMonth(year, month) ; i++) {
-				System.out.printf("%3d", i + 1);
-				if ((cnt) % 7 == 0) {
-					System.out.println();
-					cnt=0;
-				}
-				cnt++;
-		}	
+
+		cnt = weekdayint + 1;
+
+		for (int i = 0; i < getMaxDaysOfMonth(year, month); i++) {
+			System.out.printf("%3d", i + 1);
+			if ((cnt) % 7 == 0) {
+				System.out.println();
+				cnt = 0;
+			}
+			cnt++;
+		}
 
 //		for (int i = 0; i < getMaxDaysOfMonth(year, month); i++) {
 //			System.out.printf("%3d", i + 1);
--- src/honex/calendar/Prompt.java
+++ src/honex/calendar/Prompt.java
@@ -16,6 +16,7 @@ public class Prompt {
 	private final static String PROMPT_WEEKDAY="WEEKDAY(SU,MO,TU,WE,TH,FR,SA)> ";
 	
 	public void runPrompt() { 
+		//weekday를 입력받지 않고 자동계산하여 출력한다 
 		//숫자 입력받아 그달의 최대일수 출력
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
@@ -36,14 +37,17 @@ public class Prompt {
 			if (month>12) { 
 				continue;
 			}
-			System.out.print(PROMPT_WEEKDAY);
-			String weekday = scanner.next();
-			int weekdayint = cal.intWeekDay(weekday.toUpperCase());
-			if (weekdayint==-1) {
-				System.out.println("잘못된 요일을 입력했습니다");
-				break;
-			}
 			
+			int weekdayint = 0;
+//			System.out.print(PROMPT_WEEKDAY);
+//			String weekday = scanner.next();
+//			int weekdayint = cal.intWeekDay(weekday.toUpperCase());
+//			if (weekdayint==-1) {
+//				System.out.println("잘못된 요일을 입력했습니다");
+//				break;
+//			}
+			
+			weekdayint = cal.getWeekDay(year,month,1);
 //			System.out.printf("%d 월의 최대 일수는 %d 입니다. \n", mon, cal.getMaxDaysOfMonth(mon));
 			cal.printCalendar(year, month, weekdayint);
 		}
